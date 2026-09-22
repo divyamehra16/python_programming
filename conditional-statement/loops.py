644be54ed248bcf756da039d35f2a816b4e62277
divyamehra16 <divyamehra857@gmail.com>
2026-09-23 00:08:39 +0530

    py

--- conditional-statement/loops.py
+++ conditional-statement/loops.py
@@ -5,6 +5,8 @@ while --> condition
 '''
 # while-else & for-else
 # --> 'else' chalega => when loop executed completely i.e. statement becomes false i.e. loop beech mei "break" nhi hua
+#use to check the loop-COMPLETENESS
+
 
 x=[10,20,3,40,50]
 for i in x:
@@ -29,6 +31,36 @@ sum=0
 for i in item:
     sum+=i
 print(sum)
+
+
+#break - to terminate the loop
+
+a=int(input())
+for i in range(1,a):
+    if i%2==0:
+        print('Not Prime')
+        break
+
+
+#pass - used to write the EMPTY-CODE
+
+for i in range(10):
+    if i==3 or i==5:
+        pass
+    else:
+        print('hii')
+
+
+#continue - to SKIP the next part of iteration
+
+for i in range(10):
+    if i%2==0:
+        continue
+    else:
+        print('hello')
+
+
+
 
 
 #wap to input a number and print the sum of digits
